fc92583068847bf7afec6e7f34f8820320e0cb6e
kylemackenzie <kyle@getmetrics.ca>
2016-11-01 13:00:06 -0700

    Update iframe.rb

--- jobs/iframe.rb
+++ jobs/iframe.rb
@@ -1,5 +1,5 @@
 SCHEDULER.every '5m', :first_in => 0 do |job|
-    url = "http://mywebaddressgoeshere"
+    url = "https://twitter.com/realDonaldTrump?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
     puts url
     send_event('myiframe', url: url)
 end
